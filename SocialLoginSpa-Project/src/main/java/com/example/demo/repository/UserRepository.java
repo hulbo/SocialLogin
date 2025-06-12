915ed78bb812aaa5e136800f0aaf4bcdca1d3031
hulbo <hulbo@naver.com>
2025-06-12 21:19:42 +0900

    오라클 버전으로 쿼리 직접 입력

--- SocialLoginSpa-Project/src/main/java/com/example/demo/repository/UserRepository.java
+++ SocialLoginSpa-Project/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.UserEntity;
@@ -10,6 +11,7 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
 	UserEntity findByUsername(String username);
 
+	@Query("SELECT COUNT(t.id) > 0 FROM UserEntity t WHERE t.username = ?1")
 	Boolean existsByUsername(String username);
 
 	UserEntity findByUsernameAndPassword(String username, String password);
